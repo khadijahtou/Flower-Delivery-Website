cf5fad4c8f576cffe4a2da32e9823d46cbd8b4c9
khadijahtou <khadijahtouu@gmail.com>
2025-07-29 23:08:22 +0100

    add database

--- src/server.js
+++ src/server.js
@@ -1,8 +1,34 @@
-import express from "express";
+import express, { urlencoded } from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import mongoose from "mongoose";
+
+// import connectToDataBase from "./database";
+dotenv.config();
+
+async function connectToDataBase() {
+	try {
+		const mongodbUri = process.env.MONGODB_URI;
+		const connection = await mongoose.connect(mongodbUri, {
+			dbName: "flowerdeliverydb",
+		});
+		if (connection.connection.db) {
+			console.log(colors.cyan("database connected"));
+		}
+	} catch (error) {
+		console.log(colors.red(error));
+	}
+}
+const port = process.env.PORT;
 const app = express();
+// middlewear converts request to js objects
+app.use(express.json());
+//parse url
+app.use(express.urlencoded({ extended: false }));
 app.get("/end", (req, res) => {
 	res.send("hello");
 });
-app.listen(3000, () => {
-	console.log("server is runing on port 3000");
+app.listen(port, async () => {
+	console.log(colors.blue("server is running on port 3000"));
+	await connectToDataBase();
 });
