91f78dc9c7c549f2698130517f118b7f2a26b701
khadijahtou <khadijahtouu@gmail.com>
2025-08-07 13:46:06 +0100

    add userControllers

---
+++ src/controllers/userController.js
@@ -0,0 +1,32 @@
+import User from "../models/userModel.js";
+import express from "express";
+
+export const getUser = async (req, res) => {
+	try {
+		const user = await User.find();
+		res.status(200).json(user);
+	} catch (error) {
+		res.status(500).json({ message: "User not found" });
+	}
+};
+export const addUser = async (req, res) => {
+	try {
+		const { name, email, password, cartData } = req.body;
+		const user = await User.create({ name, email, password });
+		res.status(201).json(user);
+	} catch (error) {
+		res.status(500).json({ message: "user not added" });
+	}
+};
+export const deleteUser = async (req, res) => {
+	try {
+		const { id } = req.params;
+		const user = await User.findByIdAndDelete(id);
+		if (!user) {
+			return res.status(404).json({ message: "User not found" });
+		}
+		res.status(200).json({ message: "User deleted successfully" });
+	} catch (error) {
+		res.status(500).json({ message: "Error deleting user" });
+	}
+};
---
+++ src/routers/userRouter.js
@@ -0,0 +1,10 @@
+import { Router } from "express";
+import { addUser, deleteUser, getUser } from "../controllers/userController";
+
+const router = Router();
+
+router.get("/users", getUser);
+router.post("/users", addUser);
+router.delete("/users/:id", deleteUser);
+
+export default router;
