7c0387909ab219566009335b36b91c5ef5497096
khadijahtou <khadijahtouu@gmail.com>
2025-10-11 13:23:08 +0100

    add sign in

--- flower-delivery-frontend/src/Pages/CheckOut/payment.jsx
+++ flower-delivery-frontend/src/Pages/CheckOut/payment.jsx
@@ -1,16 +1,21 @@
 import React from "react";
 import Breadcrumb from "../../components/breadcrumbs";
+import { IoCheckmark } from "react-icons/io5";
+import { FiEdit2 } from "react-icons/fi";
+import { FaApple } from "react-icons/fa";
+import { FcGoogle } from "react-icons/fc";
+import OrderSummary from "./orderSummary";
 
 function Payment() {
   return (
-    <div>
-      <div className="max-w-2xl mx-auto p-6 bg-white rounded-md">
+    <div className="flex flex-col-reverse lg:grid lg:grid-cols-2">
+      <div className=" px-4 py-7 md:py-10 md:px-20 bg-white rounded-md">
         <Breadcrumb />
 
         {/* Completed Sections */}
         <div className="flex items-center justify-between border-b pb-2 mb-4">
           <div className="flex items-center gap-2">
-            <IoCheckmark className="text-green-600 text-xl" />
+            <IoCheckmark className="text-black-600 text-xl" />
             <p className="text-gray-800 font-medium">Contact information</p>
           </div>
           <button className="text-gray-500 hover:text-black text-lg">
@@ -61,7 +66,7 @@ function Payment() {
             MAKE A PURCHASE
           </button>
 
-          <div className="text-center text-sm text-gray-500 mt-4">
+          <div className="text-left text-sm text-gray-500 mt-4">
             Or pay using:
           </div>
 
@@ -71,6 +76,7 @@ function Payment() {
               className="flex items-center justify-center gap-2 border border-gray-300 rounded-md py-2 hover:border-black transition-all"
             >
               <FaApple className="text-lg" />
+
               <span className="text-sm font-medium">APPLE PAY</span>
             </button>
 
@@ -78,11 +84,14 @@ function Payment() {
               type="button"
               className="flex items-center justify-center gap-2 border border-gray-300 rounded-md py-2 hover:border-black transition-all"
             >
-              <FaGoogle className="text-lg text-[#EA4335]" />
+              <FcGoogle className="text-lg text-[#EA4335]" />
               <span className="text-sm font-medium">GOOGLE PAY</span>
             </button>
           </div>
         </form>
+      </div>
+      <div>
+        <OrderSummary />
       </div>
     </div>
   );
--- flower-delivery-frontend/src/components/signIn.jsx
+++ flower-delivery-frontend/src/components/signIn.jsx
@@ -1,7 +1,32 @@
 import React from "react";
 
 function SignIn() {
-  return <div>signIn</div>;
+  return (
+    <div className="lg:grid lg:grid-cols-3 flex flex-col gap-6">
+      <div className="flex flex-col gap-6 px-4 py-10 md:p-20 ">
+        <div>
+          <h2 className="font-semibold md:text-[50px] text-[34px]">
+            Greeting! Welcome to luxury gift shop.
+          </h2>
+        </div>
+        <div className="flex flex-col gap-2 ">
+          <h3 className="text-[16px]">
+            Use your mobile number to sign up or log in
+          </h3>
+          <input
+            type="number"
+            className="border border-gray-200 h-14 px-4 w-full"
+            placeholder="+380 xx xxx xx xx"
+          />
+          <button className="w-full h-14 bg-black text-white">Continue</button>
+        </div>
+        <div>
+          <h5 className="text-[16px]">Instantly login or sign up via Google</h5>
+          <div></div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default SignIn;
