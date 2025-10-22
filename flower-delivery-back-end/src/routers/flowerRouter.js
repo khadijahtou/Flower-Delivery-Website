db3f78d23cdbd6d1139d42d5ca46821fa2794b7a
khadijahtou <khadijahtouu@gmail.com>
2025-10-22 23:39:31 +0100

    connect admin add flower

--- flower-delivery-back-end/src/routers/flowerRouter.js
+++ flower-delivery-back-end/src/routers/flowerRouter.js
@@ -1,15 +1,16 @@
 import { Router } from "express";
 import {
-	addFlower,
-	deleteFlower,
-	getFlower,
+  addFlower,
+  deleteFlower,
+  getFlowers,
+  getFlower,
 } from "../controllers/flowerController.js";
 import upload from "../middlewares/uploadMiddleware.js";
 
 const router = Router();
 router.post("/", upload.single("image"), addFlower);
-router.get("/", getFlower);
+router.get("/", getFlowers);
+router.get("/:id", getFlower);
 router.delete("/:id", deleteFlower);
-// router.get("/:id")
 
 export default router;
--- flower-delivery-back-end/src/server.js
+++ flower-delivery-back-end/src/server.js
@@ -5,21 +5,31 @@ import flowerRouter from "./routers/flowerRouter.js";
 import userRouter from "./routers/userRouter.js";
 import authRouter from "./routers/authRouter.js";
 import connectToDataBase from "./database.js";
+import User from "./models/userModel.js";
+import { createAdmin } from "./util.js";
+import cors from "cors";
 
 dotenv.config();
 
 const port = process.env.PORT || 3000;
 const app = express();
+app.use(
+  cors({
+    origin: "*", // Allow all origins
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  })
+);
 // middlewear converts request to js objects
 app.use(express.json());
 //parse url
 app.use(express.urlencoded({ extended: false }));
 //test route
 app.get("/end", (req, res) => {
-	res.status(200).json({ messaage: "hello" });
+  res.status(200).json({ messaage: "hello" });
 });
 app.get("/", (req, res) => {
-	res.status(200).json({ message: "Welcome to the Flower Delivery API" });
+  res.status(200).json({ message: "Welcome to the Flower Delivery API" });
 });
 // user routes
 app.use("/api/flowers", flowerRouter);
@@ -27,6 +37,7 @@ app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 
 app.listen(port, async () => {
-	console.log(colors.blue("server is running on port 3000"));
-	await connectToDataBase();
+  console.log(colors.blue("server is running on port 3000"));
+  await connectToDataBase();
+  createAdmin();
 });
